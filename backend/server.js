4932df41a66bc63a93cbbcabaf28acb20799d798
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2025-04-10 12:26:54 +0530

    All Changes Done

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,7 @@ import * as aiService from "./services/ai.service.js";
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://airis-1.onrender.com", "http://localhost:5173"],
-    credentials: true,
+    origin: "*",
   },
 });
 
